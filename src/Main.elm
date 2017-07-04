b72c93e06f40baf31b2acf6b5b9d8eb8bc5f733a
bakuzan <imannoyed4days@hotmail.co.uk>
2017-07-04 21:57:49 +0100

    todo list setup...

--- src/Main.elm
+++ src/Main.elm
@@ -1,109 +1,187 @@
 module Main exposing (..)
 
-import Html exposing (Html, Attribute, text, div, ul, label, input)
-import Html.Attributes exposing (id, type_, name, for, value, class, placeholder)
-import Html.Events exposing (on, onInput, keyCode)
+import Html exposing (Html, Attribute, div, ul, label, span, li, a, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (on, onInput, onClick, keyCode)
+import Tuple exposing (first, second)
 import Json.Decode as Json
 
 
----- MODEL ----
+main =
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
+-- MODEL
+
 
 type alias Task =
-    { id: Int
-    , description: String
-    , isComplete: Bool
+    { id : Int
+    , description : String
+    , isComplete : Bool
     }
+
 
 type alias Model =
-    { uid: Int
-    , description: String
-    , taskList: List Task
+    { uid : Int
+    , description : String
+    , tasks : List Task
+    , visibilityFilter : String
     }
 
-emptyState : Model
-emptyState =
-    { uid = 0
-    , description = ""
-    , taskList = []
-    }
 
-newTask : String -> Int -> Task
-newTask desc id =
+model : Model
+model =
+    Model 3 "" initStartTasks "all"
+
+
+initStartTasks : List Task
+initStartTasks =
+    List.map mapTask startingTasks
+
+
+startingTasks : List ( Int, String )
+startingTasks =
+    [ ( 0, "Buy milk" )
+    , ( 1, "Read paper" )
+    , ( 2, "Post letter" )
+    ]
+
+
+mapTask : ( Int, String ) -> Task
+mapTask obj =
+    taskInit (first obj) (second obj)
+
+
+taskInit : Int -> String -> Task
+taskInit id description =
     { id = id
-    , description = desc
+    , description = description
     , isComplete = False
     }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
 
-
-
----- UPDATE ----
+-- UPDATE
 
 
 type Msg
-    = NoOp
-    | UpdateField
-    | CompleteTask
+    = DescriptionChange String
     | Add
+    | ApplyFilter String
+    | ToggleStatus Int Bool
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp ->
-      model ! []
+    case msg of
+        DescriptionChange newContent ->
+            { model | description = newContent }
 
-    Add ->
-        { model
-            | uid = model.uid + 1
-            , field = ""
-            , taskList =
-                if String.isEmpty model.description then
-                    model.taskList
-                else
-                    model.taskList ++ [ newTask model.description model.uid ]
-        }
-            ! []
+        Add ->
+            { model
+                | uid = model.uid + 1
+                , description = ""
+                , tasks =
+                    if String.isEmpty model.description then
+                        model.tasks
+                    else
+                        model.tasks ++ [ taskInit model.uid model.description ]
+            }
 
-    UpdateField str ->
-        { model | description = str }
-            ! []
+        ApplyFilter filter ->
+            { model | visibilityFilter = filter }
 
-    CompleteTask ->
-      ( model, Cmd.none )
+        ToggleStatus id isCompleted ->
+            let
+               updateEntry t =
+                   if t.id == id then
+                       { t | isComplete = isCompleted }
+                   else
+                       t
+            in
+                { model | tasks = List.map updateEntry model.tasks }
 
 
----- VIEW ----
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
-    div [ id "elm-practice" ]
-        [ viewInput model.description
-        , div [ class "list-container" ]
-          [ ul [ id "todo-list" ]
-            []
-          ]
+    div [ class "center-contents flex-column" ]
+        [ viewInputBox model
+        , div [] [ viewTasks model ]
+        , viewFilters model.visibilityFilter
         ]
 
-viewInput : String -> Html Msg
-viewInput desc =
-    div []
-        [ input
-          [ type_ "text"
-          , placeholder "What needs to be done?"
-          , name "description"
-          , id "description"
-          , value desc
-          , onInput UpdateField
-          , onEnter Add
-          ] []
-        , label [ for "description" ] [ text "enter a description" ]
+
+viewInputBox : Model -> Html Msg
+viewInputBox model =
+    div [ class "has-float-label" ]
+        [ input [ type_ "text", placeholder " ", value model.description, onInput DescriptionChange, onEnter Add ] []
+        , label [] [ text "What do you need to do?" ]
         ]
+
+
+viewTasks : Model -> Html Msg
+viewTasks model =
+    let
+        liItems =
+            List.map viewTaskItem (List.filter (taskFilter model.visibilityFilter) model.tasks)
+    in
+        ul [ id "task-list" ]
+            ([] ++ liItems)
+
+
+taskFilter : String -> Task -> Bool
+taskFilter filter =
+    filterTasks filter
+
+filterTasks : String -> Task -> Bool
+filterTasks filter task =
+    case filter of
+         "active" ->
+             not task.isComplete
+
+         "complete" ->
+             task.isComplete
+
+         _ ->
+            True
+
+viewTaskItem : Task -> Html Msg
+viewTaskItem task =
+    li [ class "list-item", id ("task-" ++ toString task.id) ]
+        [ label [ class "list-item__checkbox tickbox" ]
+            [ input [ type_ "checkbox", checked task.isComplete, onClick (ToggleStatus task.id (not task.isComplete)) ] []
+            , span [] [ text task.description ]
+            ]
+        ]
+
+
+viewFilters : String -> Html Msg
+viewFilters filter =
+    div [ class "filters" ]
+       [ visibilityFilter "all" filter
+       , text " "
+       , visibilityFilter "active" filter
+       , text " "
+       , visibilityFilter "complete" filter
+       ]
+
+visibilityFilter: String -> String -> Html Msg
+visibilityFilter filter activeFilter =
+    a [ href ("#/" ++ filter), classList [("selected", filter == activeFilter)], onClick (ApplyFilter filter) ]
+      [ text (capitalise filter) ]
+
+
+capitalise: String -> String
+capitalise str =
+  (String.left 1 str
+      |> String.toUpper) ++
+  String.slice 1 (String.length str) str
 
 onEnter : Msg -> Attribute Msg
 onEnter msg =
@@ -115,17 +193,3 @@ onEnter msg =
                 Json.fail "not ENTER"
     in
         on "keydown" (Json.andThen isEnter keyCode)
-
-
-
----- PROGRAM ----
-
-
-main : Program Never Model Msg
-main =
-    Html.program
-        { view = view
-        , init = init
-        , update = update
-        , subscriptions = \_ -> Sub.none
-        }
